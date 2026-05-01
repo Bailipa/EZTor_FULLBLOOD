4a59cace9c564be29ec4a738acf8d2ceb36e3289
MacBook <no_need_care@gmail>
2026-05-01 18:36:58 +0800

    chore: cleanup old migrations, docs, and config files

--- prisma/migrations/20260404120215_add_request_hash/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "TranslationRecord" ADD COLUMN "requestHash" TEXT;
-
--- CreateIndex
-CREATE INDEX "TranslationRecord_requestHash_idx" ON "TranslationRecord"("requestHash");
